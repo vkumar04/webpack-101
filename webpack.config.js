0b5037e29e91bb60fecf256c7087b20b8622c48a
Vick Kumar <vick.kumar44@gmail.com>
2017-04-15 19:01:10 -0400

    added extract text plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: './src/app.js',
@@ -8,14 +9,21 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			{test: /\.scss$/, use: ['style-loader','css-loader','sass-loader']}
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader','sass-loader']
+				})
+			}
 		]
 	},
 	plugins: [
+		new ExtractTextPlugin('app.css'),
 		new HtmlWebpackPlugin({
 			title: 'Webpack 101',
 			minify: {
-				collapseWhiteSpace: true
+				collapseWhiteSpace: false
 			},
 			hash: true,
 			template: './src/index.ejs'
